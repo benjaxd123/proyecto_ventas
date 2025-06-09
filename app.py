6190bcdb4fca376fc13def7974c129d9d84cc4b0
samueleliazar <samu.reyes@duocuc.cl>
2025-06-09 16:48:30 -0400

    nuevohtml

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = create_app()
 @app.route('/')
 def home():
     return render_template('index.html')
-
+@app.route('/agregar')
+def agregar():
+    return render_template('agregar.html')
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
